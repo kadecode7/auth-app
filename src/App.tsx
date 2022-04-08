50068fe85e701d4f88b3fce758b8a20eca1e9ecd
Kader Diarra <kader_invictus@kaders-macbook-pro.home>
2022-04-08 23:19:47 +0200

    Setup MUI 5 🚀

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-// TODO: setup eslint
-// TODO: setup husky
 
 function App() {
   return <div className="App">Hello world</div>;
