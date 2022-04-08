b13da96fe7bd1ca8b31c6b43cdc6344e517be4b5
Kader Diarra <kader_invictus@kaders-macbook-pro.home>
2022-04-08 22:45:21 +0200

    Init react project 🚀 Setup: typescript - storybook

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// TODO: setup eslint
+// TODO: setup husky
+
+function App() {
+  return <div className="App">Hello world</div>;
+}
+
+export default App;
