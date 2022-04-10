504c9ae10f2d0ceae273aa77474591bad3d1571b
Kader Diarra <kader_invictus@kaders-macbook-pro.home>
2022-04-10 23:43:43 +0200

    Create storybook component: Divider ✂ Creation of 12 stories 😎

--- src/components/Divider/Divider.stories.tsx
+++ src/components/Divider/Divider.stories.tsx
@@ -11,3 +11,85 @@ const Template: Story<Props> = (args) => <Divider {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {} as Props;
+
+export const WithContent = Template.bind({});
+WithContent.args = {
+  children: 'Center',
+} as Props;
+
+export const Colorful = Template.bind({});
+Colorful.args = {
+  children: 'Or',
+  dividerColor: '#ffb000',
+} as Props;
+
+export const TextColor = Template.bind({});
+TextColor.args = {
+  children: 'Divider',
+  textColor: '#ffb000',
+} as Props;
+
+export const AlignText = Template.bind({});
+AlignText.args = {
+  children: 'Right',
+  textAlign: 'right',
+} as Props;
+
+export const Variant = Template.bind({});
+Variant.args = {
+  children: 'Center',
+  variant: 'middle',
+} as Props;
+
+export const Length = Template.bind({});
+Length.args = {
+  dividerLength: '2rem',
+} as Props;
+
+export const DividerType = Template.bind({});
+DividerType.args = {
+  dividerType: 'dashed',
+} as Props;
+
+export const Width = Template.bind({});
+Width.args = {
+  dividerWidth: 'thick',
+} as Props;
+
+export const Position = Template.bind({});
+Position.args = {
+  dividerPosition: {
+    top: '10rem',
+  },
+} as Props;
+
+export const Horizontal = Template.bind({});
+Horizontal.args = {
+  children: 'Center',
+  orientation: 'horizontal',
+} as Props;
+
+export const Vertical = Template.bind({});
+Vertical.args = {
+  children: 'Center',
+  orientation: 'vertical',
+  flexItem: true,
+} as Props;
+Vertical.decorators = [
+  (Story) => {
+    const content = (
+      <div>
+        {`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id dignissim justo.
+     Nulla ut facilisis ligula. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+     Sed malesuada lobortis pretium.`}
+      </div>
+    );
+    return (
+      <div style={{ display: 'flex' }}>
+        {content}
+        <Story />
+        {content}
+      </div>
+    );
+  },
+];
--- src/components/Divider/index.tsx
+++ src/components/Divider/index.tsx
@@ -1,7 +1,111 @@
 import React from 'react';
+import { Divider as MuiDivider } from '@mui/material';
+import { LengthUnits } from '@src/types';
 
-export type Props = {};
+export type Props = {
+  /**
+   * divider content
+   */
+  children?: JSX.Element | string;
+  /**
+   * divider orientation
+   */
+  orientation?: 'horizontal' | 'vertical';
+  /**
+   * divider content element alignment
+   */
+  textAlign?: 'center' | 'left' | 'right';
+  /**
+   * divider variant
+   */
+  variant?: 'fullWidth' | 'inset' | 'middle';
 
-export const Divider = (props: Props): JSX.Element => {
-  return <div>Hello World Divider 👋</div>;
+  /**
+   * If true, a vertical divider will have the correct height when used in flex container.
+   */
+  flexItem?: boolean;
+
+  /**
+   * divider content element color
+   */
+  textColor?: string;
+  /**
+   * divider color
+   */
+  dividerColor?: string;
+  /**
+   * divider length
+   */
+  dividerLength?: LengthUnits | 'auto';
+  /**
+   * divider line style
+   */
+  dividerType?: 'solid' | 'dotted' | 'inset' | 'dashed' | 'groove' | 'none';
+  /**
+   * divider height
+   */
+  dividerHeight?: 'thin' | 'thick' | 'medium' | 'auto' | LengthUnits;
+  /**
+   * divider position (margin)
+   */
+  dividerPosition?: {
+    left?: LengthUnits;
+    right?: LengthUnits;
+    top?: LengthUnits;
+    bottom?: LengthUnits;
+  };
+};
+
+export const Divider = ({
+  children,
+  textColor,
+  orientation,
+  dividerColor,
+  dividerHeight,
+  dividerType,
+  dividerLength,
+  dividerPosition,
+  ...props
+}: Props): JSX.Element => {
+  const dividerStyle = children
+    ? {
+        '&.MuiDivider-root::before, &.MuiDivider-root::after': {
+          //* Divider with text
+          //* borderTop: `5px solid green` => example
+          borderTopWidth: dividerHeight,
+          borderTopStyle: dividerType,
+          borderTopColor: dividerColor,
+          marginBottom: dividerPosition?.bottom,
+          marginTop: dividerPosition?.top,
+          marginLeft: dividerPosition?.left,
+          marginRight: dividerPosition?.right,
+        },
+      }
+    : {
+        '&.MuiDivider-root': {
+          //* Divider without text
+          //* border: `5px solid green` => example
+          borderBottomWidth: dividerHeight,
+          borderStyle: dividerType,
+          borderColor: dividerColor,
+          marginBottom: dividerPosition?.bottom,
+          marginTop: dividerPosition?.top,
+          marginLeft: dividerPosition?.left,
+          marginRight: dividerPosition?.right,
+        },
+      };
+
+  return (
+    <MuiDivider
+      orientation={orientation}
+      sx={{
+        color: textColor,
+        width: dividerLength,
+        ...dividerStyle,
+      }}
+      {...props}
+    >
+      {children}
+    </MuiDivider>
+  );
 };
